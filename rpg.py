5ea5af11a736385c3c71319ae355ea34ea7a67fb
Mentor-Laptop <kamil.dmytrenko@gmail.com>
2017-04-20 13:55:24 +0200

    Board and player

--- rpg.py
+++ rpg.py
@@ -1,5 +1,47 @@
+from random import randint
+from time import sleep
+import os
+
+def create_board(width,height):
+	board = []
+
+	for row in range(0,height):
+		board_row = []
+		for column in range(0,width):
+			if row == 0 or row == height-1:
+				board_row.append("X")
+			else:
+				if column == 0 or column == width -1:
+					board_row.append("X")
+				else:
+					board_row.append(" ")
+		board.append(board_row)
+
+	return board
+
+def print_board(board):
+	for row in board:
+		for char in row:
+			print(char, end='')
+		print()
+
+
+def insert_player(board, width, height):
+	board[height][width] = '@'
+	return board
+
+
+def main():
+#	x = int(input("gimme x baby"))
+#	y = int(input("gimme y baby"))
+
+	for step in range(0,10):
+		#.system("clear")
+		board = create_board(60,20)
+		board_with_player = insert_player(board, 5 + step, 15)
+		print_board(board_with_player)
+		sleep(0.1)
 
 
 
-
-print("helloword XD")
+main()

53c3b018fb38f4e800cea82b32bf67d0e7fb1274
Mentor-Laptop <jkkrutak@gmail.com>
2017-04-21 11:21:22 +0200

    Added some basic stuff like inventory. Nothing important

--- rpg.py
+++ rpg.py
@@ -3,6 +3,24 @@ from time import sleep
 import os
 
 
+def getch():
+    import sys
+    import tty
+    import termios
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
+
+
+def print_inventory(item_to_add):
+    ''' '''
+    pass
+
 def create_board(width, height):
     board = []
 
@@ -33,20 +51,6 @@ def insert_player(board, width, height):
     return board
 
 
-def getch():
-    import sys
-    import tty
-    import termios
-    fd = sys.stdin.fileno()
-    old_settings = termios.tcgetattr(fd)
-    try:
-        tty.setraw(sys.stdin.fileno())
-        ch = sys.stdin.read(1)
-    finally:
-        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-    return ch
-
-
 def x_movement(ch):
     if ch == 'a':
         return -1
@@ -73,18 +77,23 @@ def force_exit(ch):
 def main():
     x_pos = 15
     y_pos = 15
+    zycie = 5
+    inventory = ["zbroja mocna w chuj +200 def", "klucz do drzwi erkado", "krzeslo do ubicia matki jak w tibi + 500 do ataku matek"]
     while True:
         character = getch()
+        #if character == spacja:
+        #    attack()
+
         force_exit(character)
         os.system('clear')
-        board = create_board(100, 30)  # Średnio bo średnio ale działa
-        board_with_player = insert_player(board, x_pos + x_movement(character), y_pos + y_movement(character) )
+        board = create_board(40, 40)  # Średnio bo średnio ale działa
         if board[x_pos + x_movement(character)][y_pos + y_movement(character)] != 'X':
+            board_with_player = insert_player(board, x_pos + x_movement(character), y_pos + y_movement(character) )
             x_pos = x_pos + x_movement(character)
             y_pos = y_pos + y_movement(character)
             print_board(board_with_player)
         else:
             print_board(board_with_player)
-
-
+        print("Zycie:", zycie, "inventory:", str(' '.join(inventory[0:])))
+        zycie -= 1
 main()

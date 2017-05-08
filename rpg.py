1abb4cc63def27dca218dfbbc9ef0fcf9bf1660d
Mentor-Laptop <jkkrutak@gmail.com>
2017-05-08 15:08:58 +0200

    Random generated lands

--- rpg.py
+++ rpg.py
@@ -1,20 +1,20 @@
-from random import randint
+import random
 from time import sleep
 import os
 
-
 def getch():
-    import sys
-    import tty
-    import termios
-    fd = sys.stdin.fileno()
-    old_settings = termios.tcgetattr(fd)
-    try:
-        tty.setraw(sys.stdin.fileno())
-        ch = sys.stdin.read(1)
-    finally:
-        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-    return ch
+   import sys
+   import tty
+   import termios
+   fd = sys.stdin.fileno()
+   old_settings = termios.tcgetattr(fd)
+   try:
+       tty.setraw(sys.stdin.fileno())
+       ch = sys.stdin.read(1)
+   finally:
+       termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+   return ch
+
 
 
 def print_inventory(item_to_add):
@@ -23,6 +23,7 @@ def print_inventory(item_to_add):
 
 def create_board(width, height):
     board = []
+    line = 0
 
     for row in range(0, height):
         board_row = []
@@ -36,6 +37,31 @@ def create_board(width, height):
                     board_row.append(".")
         board.append(board_row)
 
+    with open('map.txt', 'r') as maps:
+        for row in maps:
+            line += 1
+            if line % 2 == 0:
+                x_generator = row
+            else:
+                y_generator = row
+
+    x_generator = int(x_generator)
+    y_generator = int(y_generator)
+    board[x_generator][y_generator] = 'X'
+    board[x_generator-1][y_generator] = 'A'
+    board[x_generator+1][y_generator] = 'B'
+    board[x_generator][y_generator+1] = 'C'
+    board[x_generator][y_generator-1] = 'D'
+    board[x_generator-1][y_generator-1] = 'E'
+    board[x_generator+1][y_generator+1] = 'F'
+    board[x_generator+1][y_generator-1] = 'G'
+    board[x_generator-1][y_generator+1] = 'H'
+
+
+
+
+    #board = create_lands(board,width,height)
+
     return board
 
 
@@ -47,9 +73,20 @@ def print_board(board):
 
 
 def insert_player(board, width, height):
-    board[height][width] = '@'
+    board[height][width] = "@"
     return board
 
+def insert_mob(board, width, height):
+    board[height][width] = 'X'
+    return board
+
+def generate_lands(width, height, count = 5):
+    y_generator = random.randrange(0,width)
+    x_generator = random.randrange(0,height)
+
+    with open('map.txt', 'a') as out:
+        out.write(str(x_generator)+'\n'+str(y_generator)+'\n')
+    
 
 def x_movement(ch):
     if ch == 'a':
@@ -75,10 +112,12 @@ def force_exit(ch):  # tymczasowy exit do fazy testów
 
 
 def health(hp):
-    health = []
-    for i in range(0, hp):
-        health.append("♥")
-    return health
+   health = []
+   for i in range(0, hp):
+       health.append("♥")
+   return health
+
+
 
 
 def main():
@@ -86,18 +125,26 @@ def main():
     y_position = 15
     life = 5
     inventory = []
+    #board = create_board(80,30)
+    generate_lands(25,25)
+
     while True:
         character = getch()
         force_exit(character)
         os.system('clear')
-        board = create_board(80, 30)
+        board = create_board(80,30)
+        board_with_player = insert_mob(board,5,5)
         if not board[y_position + y_movement(character)][x_position + x_movement(character)] == 'X':
-            board_with_player = insert_player(board, x_position + x_movement(character), y_position + y_movement(character))
             x_position = x_position + x_movement(character)
             y_position = y_position + y_movement(character)
-            print_board(board_with_player)
-        else:
-            print_board(board_with_player)
+        board_with_player = insert_player(board, x_position, y_position)
+        print_board(board_with_player)
+
+
+
+
+
+
         print("Life:", str(''.join(health(life)))), "Inventory:", str(' '.join(inventory[0:]))
 
 
